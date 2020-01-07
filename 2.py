cfdfd4ab5a455c7479db92d7299bfb9337fcc9e6
shenjialing <shen_jialing@126.com>
2020-01-07 11:11:43 +0800

    20200107

--- 2.py
+++ 2.py
@@ -1,7 +1,19 @@
-def addTwoNumbers(l1, l2):
-    for i in l1:
-        print(l1[i])
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-l1 = [2,4,3]
-l2 = [5,6,4]
-addTwoNumbers(l1,l2)
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        if not l1:
+            return l2
+        elif not l2:
+            return l1
+        else :
+            val = l1.val + l2.val
+            result = ListNode(val%10)
+            result.next = self.addTwoNumbers(l1.next,l2.next)
+            if(val>9):
+                result.next = self.addTwoNumbers(ListNode(1),result.next)
+            return result
